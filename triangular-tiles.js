65e48ca01d9b8f46c9da989c3e8136d69c98ad52
Jānis Lībeks <libeks@gmail.com>
2026-01-18 17:04:35 -0500

    fix link style, add Further Ideas section

--- triangular-tiles.js
+++ triangular-tiles.js
@@ -42,6 +42,10 @@ const tricurve35 = {
   template: `<path class="stroke" d="M 33.3 28.87 C 16.6 19.24, 0 48.1, -16.6 57.78" /> <!-- 35 -->`,
 }
 
+const tricurve36 = {
+  template: `<path class="stroke" d="M 33.3 28.87 C 16.6 19.24, -16.6 19.24, -33.3 28.87" /> <!-- 36 -->`,
+}
+
 const tricurve45 = {
   template: `<path class="stroke" d="M 16.6 57.78 Q 0 48.1, -16.6 57.78" /> <!-- 45 -->`,
 }
@@ -52,6 +56,202 @@ const tricurve46 = {
 
 const tricurve56 = {
   template: `<path class="stroke" d="M -33.3 28.87 C -16.6 19.24, 0 48.1, -16.6 57.78" /> <!-- 56 -->`,
+}
+
+const tile123456 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve12 />
+    <tricurve34 />
+    <tricurve56 />
+  </g>`,
+  components: {
+    tricurve12,
+    tricurve34,
+    tricurve56,
+  },
+}
+
+const tile123546 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve12 />
+    <tricurve35 />
+    <tricurve46 />
+  </g>`,
+  components: {
+    tricurve12,
+    tricurve35,
+    tricurve46,
+  },
+}
+
+const tile123645 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve12 />
+    <tricurve36 />
+    <tricurve45 />
+  </g>`,
+  components: {
+    tricurve12,
+    tricurve36,
+    tricurve45,
+  },
+}
+
+const tile132456 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve13 />
+    <tricurve24 />
+    <tricurve56 />
+  </g>`,
+  components: {
+    tricurve13,
+    tricurve24,
+    tricurve56,
+  },
+}
+
+const tile132546 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve13 />
+    <tricurve25 />
+    <tricurve46 />
+  </g>`,
+  components: {
+    tricurve13,
+    tricurve25,
+    tricurve46,
+  },
+}
+
+const tile132645 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve13 />
+    <tricurve26 />
+    <tricurve45 />
+  </g>`,
+  components: {
+    tricurve13,
+    tricurve26,
+    tricurve45,
+  },
+}
+
+const tile142356 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve14 />
+    <tricurve23 />
+    <tricurve56 />
+  </g>`,
+  components: {
+    tricurve14,
+    tricurve23,
+    tricurve56,
+  },
+}
+
+const tile142536 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve14 />
+    <tricurve25 />
+    <tricurve36 />
+  </g>`,
+  components: {
+    tricurve14,
+    tricurve25,
+    tricurve36,
+  },
+}
+
+const tile142635 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve14 />
+    <tricurve26 />
+    <tricurve35 />
+  </g>`,
+  components: {
+    tricurve14,
+    tricurve26,
+    tricurve35,
+  },
+}
+
+const tile152346 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve15 />
+    <tricurve23 />
+    <tricurve46 />
+  </g>`,
+  components: {
+    tricurve15,
+    tricurve23,
+    tricurve46,
+  },
+}
+
+const tile152436 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve15 />
+    <tricurve23 />
+    <tricurve46 />
+  </g>`,
+  components: {
+    tricurve15,
+    tricurve23,
+    tricurve46,
+  },
+}
+
+const tile152634 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve15 />
+    <tricurve26 />
+    <tricurve34 />
+  </g>`,
+  components: {
+    tricurve15,
+    tricurve26,
+    tricurve34,
+  },
+}
+
+const tile162345 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve16 />
+    <tricurve23 />
+    <tricurve45 />
+  </g>`,
+  components: {
+    tricurve16,
+    tricurve23,
+    tricurve45,
+  },
+}
+
+const tile162435 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve16 />
+    <tricurve24 />
+    <tricurve35 />
+  </g>`,
+  components: {
+    tricurve16,
+    tricurve24,
+    tricurve35,
+  },
 }
 
 const tile162534 = {
@@ -68,4 +268,39 @@ const tile162534 = {
   },
 }
 
-export { tile162534 }
+const tilesetTri15 = {
+  tile123456,
+  tile123546,
+  tile123645,
+  tile132456,
+  tile132546,
+  tile132645,
+  tile142356,
+  tile142536,
+  tile142635,
+  tile152346,
+  tile152436,
+  tile152634,
+  tile162345,
+  tile162435,
+  tile162534,
+}
+
+export {
+  tile123456,
+  tile123546,
+  tile123645,
+  tile132456,
+  tile132546,
+  tile132645,
+  tile142356,
+  tile142536,
+  tile142635,
+  tile152346,
+  tile152436,
+  tile152634,
+  tile162345,
+  tile162435,
+  tile162534,
+  tilesetTri15,
+}

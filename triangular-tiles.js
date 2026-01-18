882617b6489669fb899b2fbc0868c8c506edb824
Jānis Lībeks <libeks@gmail.com>
2026-01-18 16:19:34 -0500

    Add 15 triangular tiles, start moving over to tile templates

---
+++ triangular-tiles.js
@@ -0,0 +1,71 @@
+const tricurve12 = {
+  template: `<path class="stroke" d="M -16.6 0 C -16.6 19.24, 16.6 19.24, 16.6 0" /> <!-- 12 -->`,
+}
+
+const tricurve13 = {
+  template: `<path class="stroke" d="M -16.6 0 C -16.6 19.24, 16.6 19.24, 33.3 28.87" /> <!-- 13 -->`,
+}
+
+const tricurve14 = {
+  template: `<path class="stroke" d="M -16.6 0 C -16.6 19.24, 0 48.1, 16.6 57.78" /> <!-- 14 -->`,
+}
+
+const tricurve15 = {
+  template: `<path class="stroke" d="M -16.6 0 C -16.6 19.24, 0 48.1, -16.6 57.78" /> <!-- 15 -->`,
+}
+
+const tricurve16 = {
+  template: `<path class="stroke" d="M -16.6 0 Q -16.6 19.24, -33.3 28.87" /> <!-- 16 -->`,
+}
+
+const tricurve23 = {
+  template: `<path class="stroke" d="M 16.6 0 Q 16.6 19.24, 33.3 28.87" /> <!-- 23 -->`,
+}
+
+const tricurve24 = {
+  template: `<path class="stroke" d="M 16.6 0 C 16.6 19.24, 0 48.1, 16.6 57.78" /> <!-- 24 -->`,
+}
+
+const tricurve25 = {
+  template: `<path class="stroke" d="M 16.6 0 C 16.6 19.24, 0 48.1, -16.6 57.78" /> <!-- 25 -->`,
+}
+
+const tricurve26 = {
+  template: `<path class="stroke" d="M 16.6 0 C 16.6 19.24, -16.6 19.24, -33.3 28.87" /> <!-- 26 -->`,
+}
+
+const tricurve34 = {
+  template: `<path class="stroke" d="M 16.6 57.78 C 0 48.1, 16.6 19.24, 33.3 28.87" /> <!-- 34 -->`,
+}
+
+const tricurve35 = {
+  template: `<path class="stroke" d="M 33.3 28.87 C 16.6 19.24, 0 48.1, -16.6 57.78" /> <!-- 35 -->`,
+}
+
+const tricurve45 = {
+  template: `<path class="stroke" d="M 16.6 57.78 Q 0 48.1, -16.6 57.78" /> <!-- 45 -->`,
+}
+
+const tricurve46 = {
+  template: `<path class="stroke" d="M 16.6 57.78  C 0 48.1, -16.6 19.24, -33.3 28.87" /> <!-- 46 -->`,
+}
+
+const tricurve56 = {
+  template: `<path class="stroke" d="M -33.3 28.87 C -16.6 19.24, 0 48.1, -16.6 57.78" /> <!-- 56 -->`,
+}
+
+const tile162534 = {
+  template: `<g class="tile">
+    <polygon points="0,86.6 -50,0 50,0" />
+    <tricurve16 />
+    <tricurve25 />
+    <tricurve34 />
+  </g>`,
+  components: {
+    tricurve16,
+    tricurve25,
+    tricurve34,
+  },
+}
+
+export { tile162534 }

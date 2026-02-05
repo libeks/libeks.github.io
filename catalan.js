9cf705f9096277443cec62cffcfa393f1e152397
Jānis Lībeks <libeks@gmail.com>
2026-02-05 15:09:00 -0500

    Update catalan computation to be iterative

--- catalan.js
+++ catalan.js
@@ -1,4 +1,20 @@
-function catalanNumber(n) {
+function memoize(func) {
+  // adapted from https://dev.to/jeremydmarx813/create-a-memoized-function-in-javascript-43pi
+  const cache = {};
+  return (...args) => {
+    // console.log(cache)
+     let strKey = args.join(',');
+      if(!cache[strKey]){
+        // console.log('adding to cache!');
+        cache[strKey] = func.apply(this, args);
+      } 
+       // console.log('fetching from cache!');
+       return cache[strKey];
+   }
+};
+
+function catNum(n) {
+  // console.log(`computing C${n}`)
   if (n == 0 || n == 1) {
     return 1
   }
@@ -8,6 +24,39 @@ function catalanNumber(n) {
   }
   return sum
 }
+
+const catalanNumber = memoize(catNum);
+
+// Return a balanced-parenthesis representation of the Catalan Structure Cn with index i
+const generateIterativeCatalanExpensive = function(n, i) {
+  // console.log("iterative", n, i)
+  if (n == 0) {
+    return ''
+  }
+  if (n == 1) {
+    return '()'
+  }
+  let cn = catalanNumber(n)
+  if (i>=cn) {
+    i = i % cn
+  }
+  let iter = 0;
+  for (let a1 = 0; a1 <= n-1; a1++) {
+    let ca1 = catalanNumber(a1);
+    let a2 = n-a1-1;
+    let ca2 = catalanNumber(a2);
+    for (let j=0; j < ca1; j++) {
+      if (i < ca2) {
+        const result = `(${generateIterativeCatalan(a1, j)})${generateIterativeCatalan(a2, i)}`;
+        return result;
+      }
+      i -= ca2;
+    }
+  } 
+  return 'unknown'
+}
+
+const generateIterativeCatalan = memoize(generateIterativeCatalanExpensive)
 
 function generateCatalanParenthesisSet(n) {
   if (n == 0) {
@@ -20,9 +69,11 @@ function generateCatalanParenthesisSet(n) {
   for (let i = 0; i <= n - 1; i++) {
     let left = generateCatalanParenthesisSet(i)
     let right = generateCatalanParenthesisSet(n - i - 1)
-    for (let l of left) {
-      for (let r of right) {
+    for (let [i, l] of left.entries()) {
+      for (let [j,r] of right.entries()) {
+        // console.log("AAA",retList.length, i,j)
         retList.push(['(', ...l, ')', ...r])
+
       }
     }
   }
@@ -31,6 +82,42 @@ function generateCatalanParenthesisSet(n) {
 
 function offsetArrayVals(a, val) {
   return a.map((v) => v + val)
+}
+
+function hexConversion(char) {
+  if (char < 10) {
+    return char
+  }
+  return String.fromCharCode(65 + (char - 10))
+}
+
+
+function arrayOfArrayToArrayOfNumStrings(a) {
+  console.log(a);
+  return a.map((entry) => entry.map((char) => hexConversion(char)).join(''))
+}
+
+function arrayToNumStrings(a) {
+  return a.map((char) => hexConversion(char)).join('');
+}
+
+
+// given a parenthetical representation of a Catalan structure, return the correspoding numerical string
+function parenthesesToNumerical(st) {
+  let ret = [];
+  let stack = [];
+  for (let i = st.length-1; i>=0; i--) {
+    const char = st[i]
+    if (char == ')') {
+      stack.push(i+1);
+    } else if (char == '(') {
+      const last = stack.pop();
+      ret.push([i+1, last])
+    } else {
+      throw "Unknown character"
+    }
+  }
+  return ret.reverse().flat(Infinity) // flatten nested list
 }
 
 function generateCatalanNumberSet(n) {
@@ -44,14 +131,18 @@ function generateCatalanNumberSet(n) {
   for (let i = 0; i <= n - 1; i++) {
     let left = generateCatalanNumberSet(i)
     let right = generateCatalanNumberSet(n - i - 1)
-    for (let l of left) {
-      for (let r of right) {
-        retList.push([
+    for (let [leftID, l] of left.entries()) {
+      for (let [rightID,r] of right.entries()) {
+        const gen = generateIterativeCatalan(n, retList.length);
+        console.log("generative", gen, parenthesesToNumerical(gen), arrayToNumStrings(parenthesesToNumerical(gen)))
+        const val = [
           1,
           l.length + 2,
           ...offsetArrayVals(l, 1),
           ...offsetArrayVals(r, l.length + 2),
-        ])
+        ];
+        retList.push(val)
+        // console.log("iterative", val)
       }
     }
   }
